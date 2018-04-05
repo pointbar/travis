356b46dd881100b19d2d812fc6c68188c0a4da5f
pntbr <stephane.langlois@scopyleft.fr>
2018-04-05 20:16:10 +0200

    Variable in string

--- push-data.sh
+++ push-data.sh
@@ -3,4 +3,4 @@
 url="http://localhost:8000"
 file="lanzarot"
 
-google-chrome-stable --headless --disable-gpu --dump-dom "http://localhost:8000/#"$pageName > $pageName".html"
+google-chrome-stable --headless --disable-gpu --dump-dom "http://localhost:8000/#$pageName" > "$pageName.html"
